71f154678835c74dc18f6551d9f22e91e7c1eeea
Jordan Endicott <jordan.endicott823@gmail.com>
2018-07-07 19:10:03 -0400

    started over, fail

--- tdd_encode.rb
+++ tdd_encode.rb
@@ -6,24 +6,4 @@
 # 	more.tr('F-ZA-Ef-za-e', 'A-Za-z')
 # end
 
-# def input_an_arry(new_arr)
-# 	if new_arr.class = Array
-# 		true
-# 	end
-# end
 
-# def funk(something)
-# 	main_arr = [*"a".."z"]
-# 	new_arr = something.split("")
-# 	if input_an_arry(new_arr) == true
-
-# 	end
-# end
-
-def converter(input)
-	work_arr = input.split("")
-end
-
-def encoder(input)
-	converter(input)
-end
--- test_tdd_encode.rb
+++ test_tdd_encode.rb
@@ -7,37 +7,8 @@ class TestYourEncoding < Minitest::Test
         assert_equal(1, 1)
     end
 
-    def test_assert_that_converter_returns_array
-    	assert_equal(Array, converter("").class)
+  	def test_that_who_cares
+    	assert_equal("fgh", encoder("abc"))
     end
 
-    def test_assert_that_converter_converts
-    	assert_equal(["0", "1", "2"], converter("012"))
-    end
-
-
-    def test_assert_that_converter_works_inside_encoder
-    	assert_equal(["0", "1", "2"], encoder("012"))
-    end
-
-    def test_that_converter_takes_spaces
-    	assert_equal(["h", "e" ,"l", "l", "o", " ", "w", "o", "r", "l", "d"], converter("hello world"))
-    end
-    # def test_assert_that_string_is_converted
-    # 	assert_equal(Array, ("").class)
-    # end
-
-    
-    # def test_that_a_returns_f
-    # 	assert_equal("f", funk("a"))
-    # end
-
-    # def test_that_words_encrypt
-    # 	assert_equal("fgh", funk("abc"))
-    # end
-
-    # def test_that_words_decrypt
-    # 	assert_equal("abc", bunk("fgh"))
-    # end
-    
 end
