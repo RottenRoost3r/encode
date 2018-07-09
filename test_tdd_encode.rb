5eb1ec5d3e7b08f0e6d2133c4fb55ef1c18eb9d0
Jordan Endicott <jordan.endicott823@gmail.com>
2018-07-09 11:30:37 -0400

    pass, further testing

--- test_tdd_encode.rb
+++ test_tdd_encode.rb
@@ -31,6 +31,7 @@ class TestYourEncoding < Minitest::Test
    end
 
    def test_that_message_is_prepared
+   		assert_equal("muffinman", prepare("MuffinMan"))
    		assert_equal("muffinman", prepare("Muffin Man"))
    		assert_equal("muffinman", prepare("Muffin Man!...?"))
 
