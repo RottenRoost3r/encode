ebb5a9222e24d93f5cf087dae28270bed7c55282
Jordan Endicott <jordan.endicott823@gmail.com>
2018-07-09 11:43:26 -0400

    pass, ftoe rotates

--- tdd_encode.rb
+++ tdd_encode.rb
@@ -39,4 +39,5 @@ def atoz()
 end
 
 def atoe()
+	atoz.rotate(5)
 end
--- test_tdd_encode.rb
+++ test_tdd_encode.rb
@@ -41,6 +41,6 @@ class TestYourEncoding < Minitest::Test
    	end
 
    	def test_that_atoz_rotates
-   		assert_equal(["f", "g", "h"], atoe("abc"))
+   		assert_equal(["f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e"], atoe())
    	end
 end
